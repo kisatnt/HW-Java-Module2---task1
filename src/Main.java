3e4117c4e1a15f515a14d53dee1d0a46b0de3cf0
Andrei Kiselev <eparis@atlassian.com>
2022-12-20 22:26:22 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 400;
+        int bonus = ticketCost / 20;
+        int summa = bonus;
+        System.out.println (summa);
+    }
+
+
+
+
+}
